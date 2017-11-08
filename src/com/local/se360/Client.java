ce2c8a0425e4480e5156fa8d83ac42c4f32cf6e6
ejrbuss <ejrbuss@gmail.com>
2017-11-07 23:27:58 -0800

    minor refactoring

--- src/com/local/se360/Client.java
+++ src/com/local/se360/Client.java
@@ -11,6 +11,7 @@ public final class Client extends Connector {
 	private Consumer<Status> accepter;
 	
 	public Client() {
+		
 		super();
 		
 		socket = new SocketController(Config.ADDRESS, Config.PORT);
